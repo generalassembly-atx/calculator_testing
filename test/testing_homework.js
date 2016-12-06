bbb92b1ec85a7548566df4f8db348ee671855669
Namanda Musoke <nvmusoke@gmail.com>
2016-12-05 20:59:28 -0600

    tested calculations

--- test/testing_homework.js
+++ test/testing_homework.js
@@ -1,9 +1,10 @@
 var expect = require('chai').expect;
+var randomnumber = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
 var calculator = {
   /* Basic arithmatic functions */
   add: function(a,b){ return a + b; },
   subtract: function(a,b){ return a - b; },
-  multiply: function(a,b){ return a * a; },
+  multiply: function(a,b){ return a * b; },
   divide: function(a,b){ return a / b; },
   squared: function(n){ return n * n; },
   /* Look up the Math object on W3 schools or Mozilla */
@@ -18,6 +19,8 @@ var calculator = {
   /* BONUS: write a function that returns a random number between 1-10,
   and figure out a way to test it, *Use stackOverflow* HINT: a loop when testing */
   random: function(){
+    var randomnumber;
+
   }
 };
 
@@ -27,6 +30,36 @@ describe('Calculator functions work', function(){
 
   it('calculator.add works correctly',function(){
     expect(calculator.add(1,2)).to.equal(3);
+  });
+
+  it('calculator.subtract works correctly',function(){
+    expect(calculator.subtract(4,3)).to.equal(1);
+  });
+  it('calculator.multiply works correctly',function(){
+    expect(calculator.multiply(3,2)).to.equal(6);
+  });
+  it('calculator.divide works correctly',function(){
+    expect(calculator.divide(8,2)).to.equal(4);
+  });
+  it('calculator.squared works correctly',function(){
+    expect(calculator.squared(4)).to.equal(16);
+  });
+  it('calculator.power works correctly',function(){
+    expect(Math.pow(4,3)).to.equal(64);
+  });
+  it('calculator.squareRoot works correctly',function(){
+    expect(Math.sqrt(9)).to.equal(3);
+  });
+
+  it('calculator.roundUp works correctly',function(){
+    expect(Math.ceil(1.2)).to.equal(2);
+  });
+  it('calculator.roundDown works correctly',function(){
+    expect(Math.floor(1.2)).to.equal(1);
+  });
+
+  it('calculator.random works correctly',function(){
+    expect(randomnumber).to.be.above(-1);
   });
 
 });
