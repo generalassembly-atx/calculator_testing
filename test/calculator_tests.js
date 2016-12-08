323c0f65254f90501c9a030646ef30ed3e2ad020
christopher barragan <barraganchris95@gmail.com>
2016-12-08 02:01:08 -0600

    I had to copy my hw into a new forked repo, but it is complete

--- test/calculator_tests.js
+++ test/calculator_tests.js
@@ -1,9 +1,10 @@
+//I had to copy my other hw onto a new forked repo, i messed up the first time
 var expect = require('chai').expect;
 var calculator = {
   /* Basic arithmatic functions */
   add: function(a,b){ return a + b; },
   subtract: function(a,b){ return a - b; },
-  multiply: function(a,b){ return a * a; },
+  multiply: function(a,b){ return a * b; },
   divide: function(a,b){ return a / b; },
   squared: function(n){ return n * n; },
   /* Look up the Math object on W3 schools or Mozilla */
@@ -12,21 +13,79 @@ var calculator = {
   /* E.G. squareRoot(9) is 3 */
   squareRoot: function(n){ return Math.sqrt(n); },
   /* E.G. Rounds up a number roundUp(3.1) will be "4" */
-  roundUp: function(n){ return Math.floor(n); },
+  roundUp: function(n){ return Math.ceil(n); },
   /* E.G. Rounds down a number roundUp(3.1) will be "3" */
-  roundDown: function(n){ return Math.ceil(n); },
+  roundDown: function(n){ return Math.floor(n); },
   /* BONUS: write a function that returns a random number between 1-10,
   and figure out a way to test it, *Use stackOverflow* HINT: a loop when testing */
-  random: function(){
-  }
-};
 
+  // Random Function I couldn't figure out
+  // random: function(){ return (Math.floor((Math.random() * 10) + 1; }
+};
 /* Write Mocha/Chai Tests here */
 /* Starter Code */
+
+// Add
 describe('Calculator functions work', function(){
 
   it('calculator.add works correctly',function(){
     expect(calculator.add(1,2)).to.equal(3);
   });
+
+// Subtract
+
+  it('calculator.subtract works correctly',function(){
+    expect(calculator.subtract(4,2)).to.equal(2);
+  });
+
+// Multiply
+
+  it('calculator.multiply works correctly',function(){
+    expect(calculator.multiply(2,2)).to.equal(4);
+  });
+
+// Divide
+
+  it('calculator.divide works correctly',function(){
+    expect(calculator.divide(12,2)).to.equal(6);
+  });
+
+// Squared
+
+  it('calculator.squared works correctly', function() {
+    expect(calculator.squared(2)).to.equal(4);
+  });
+
+// Power
+
+  it('calculator.power works correctly', function() {
+    expect(calculator.power(2,2)).to.equal(4);
+  });
+
+// Square Root
+
+  it('calculator.square root works correctly', function() {
+    expect(calculator.squareRoot(144)).to.equal(12);
+  });
+
+// Round Up
+
+  it('calculator.roundUp works correctly', function() {
+    expect(calculator.roundUp(3.6)).to.equal(4);
+  });
+
+// Round down
+
+  it('calculator.roundDown works correctly', function() {
+    expect(calculator.roundDown(6.4)).to.equal(6);
+  });
+
+// Random
+
+  // it('calculator.random works correctly', function() {
+  //   for (i = 0; i < 10; i++) {
+  //     expect(calculator.random()).to.equal();
+  //   }
+  // }
 
 });
